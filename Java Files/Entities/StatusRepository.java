6018f28b3b2910b347b1b2a341916e4920dbdc50
Shannon <shannonchimutengo@gmail.com>
2024-04-05 18:19:50 +0200

    Delete Java Files directory

--- Java Files/Entities/StatusRepository.java
+++
@@ -1,8 +0,0 @@
-package org.vaadin.example.Entities;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StatusRepository extends JpaRepository<Status, Long> {
-
-}
