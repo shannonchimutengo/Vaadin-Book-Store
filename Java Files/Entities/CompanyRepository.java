2c278c3ca62b6b1d6b09e54382e5108bbc124402
Shannon <shannonchimutengo@gmail.com>
2024-04-05 19:37:52 +0200

    Delete Java Files directory

--- Java Files/Entities/CompanyRepository.java
+++
@@ -1,7 +0,0 @@
-package org.vaadin.example.Entities;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyRepository extends JpaRepository<Company, Long> {
-
-}
